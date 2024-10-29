1f37c70125ca8f0f977b5ef84352fe4449937159
vikramtech87 <vikramrajtech87@gmail.com>
2024-10-29 20:49:06 +0530

    removed unsed variable

--- src/app/registrations/_components/registration-table-row.tsx
+++ src/app/registrations/_components/registration-table-row.tsx
@@ -25,20 +25,6 @@ const RegistrationTableRow = ({ registration }: RegistrationTableRowProps) => {
     }
   };
 
-  const mealPref = () => {
-    if (meal === undefined) {
-      return "Not available";
-    }
-    switch (meal) {
-      case "non-veg":
-        return "";
-      case "veg":
-        return "V";
-      default:
-        "Not available";
-    }
-  };
-
   return (
     <TableRow>
       <TableCell>{name}</TableCell>
